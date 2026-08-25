a2d96c629c654c2af58c3a416ce6637547c44972
Taj Patel <tajpatel58@gmail.com>
2026-08-25 18:14:28 +0100

    updating statement processsing to include nationwide

--- src/spending_tracker/data_processing/statements.py
+++ src/spending_tracker/data_processing/statements.py
@@ -1,19 +1,22 @@
 from pathlib import Path
 from spending_tracker.data_processing import chase
 from spending_tracker.data_processing import amex
+from spending_tracker.data_processing import nationwide
 from spending_tracker.data_processing import common
 from spending_tracker.data_ingestion import pdf_parsing
 
 
 _BANK_TO_DATA_LOADING_FUNCTIONS = {
     "Chase": chase.load_raw_chase_statement_pdf,
-    "Amex": amex.load_raw_amex_statement_csv
+    "Amex": amex.load_raw_amex_statement_csv,
+    "Nationwide": nationwide.load_raw_nationwide_statement_csv
 }
 
 
 _BANK_TO_PROCESSING_FUNCTIONS = {
     "Chase": chase.clean_chase_transaction_dataframe,
-    "Amex": amex.clean_amex_transaction_dataframe
+    "Amex": amex.clean_amex_transaction_dataframe,
+    "Nationwide": nationwide.clean_nationwide_transaction_dataframe
 }
 
 
@@ -30,8 +33,8 @@ def parse_and_clean(data_home: Path) -> list:
 
         # Clean the DataFrame
         cleaned_transactions_df = _BANK_TO_PROCESSING_FUNCTIONS[bank](raw_transactions_df, 
-                                                                      account_id=account_id, 
-                                                                      user=user)
+                                                                    account_id=account_id, 
+                                                                    user=user)
 
         # Save the cleaned DataFrame to CSV
         common.save_partitioned_dataframe_to_csv(cleaned_transactions_df, processed_transactions_data_root, 
