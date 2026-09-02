889493c5eb5daea1843af58e382eb8d0a9ad75a5
Taj Patel <tajpatel58@gmail.com>
2026-09-02 18:41:18 +0100

    adding function to ingest labelled categories

--- src/spending_tracker/database/transactions.py
+++ src/spending_tracker/database/transactions.py
@@ -1,3 +1,5 @@
+from urllib import response
+
 from supabase import Client
 import pandas as pd
 
@@ -38,3 +40,58 @@ def upload_transactions_to_db(
     )
 
     return response
+
+
+def fetch_transactions_from_db(
+    supabase_client: Client,
+    table_name: str,
+) -> pd.DataFrame:
+    """Fetch all transactions from the database."""
+    response = (
+        supabase_client
+        .table(f"{table_name}")
+        .select("*")
+        .execute()
+    )
+    if response.status_code != 200:
+        raise RuntimeError(f"Failed to fetch transactions: {response.data}")
+
+    transactions_df = pd.DataFrame(response.data)
+    return transactions_df
+
+
+def fetch_manually_categorised_transactions_as_dict(
+    supabase_client: Client,
+) -> dict:
+    """Load transactions that have been manually categorised."""
+    metadata_response = (
+        supabase_client
+        .table("transactions_metadata")
+        .select("*")
+        .is_("hidden", "null")
+        .execute()
+    )
+
+    transactions_response = (
+        supabase_client
+        .table("transactions")
+        .select("*")
+        .execute()
+    )
+
+    # convert to dataframes and merge on event_id:
+    metadata_df = pd.DataFrame(metadata_response.data)
+    transactions_df = pd.DataFrame(transactions_response.data)
+    transactions_with_metadata_df = pd.merge(transactions_df, metadata_df, on="event_id", how="inner")
+    transactions_with_metadata_df["category"] = transactions_with_metadata_df["manual_category"].fillna(transactions_with_metadata_df["llm_category"])
+
+    labelled_examples = [
+        {
+            "merchant": row["merchant"],
+            "amount": row["amount"],
+            "category": row["category"]
+        }
+        for _, row in transactions_with_metadata_df.iterrows()
+    ]
+
+    return labelled_examples
